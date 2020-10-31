98afe6b575873d627899810a2312daf73d48d6e1
wsdfhjxc <wsdfhjxc@op.pl>
2020-10-31 22:15:14 +0100

    Set up D-Bus service

--- plugin/DoItYourselfBar.cpp
+++ plugin/DoItYourselfBar.cpp
@@ -1,12 +1,38 @@
 #include "DoItYourselfBar.hpp"
 
+#include <QDBusConnection>
+#include <QDebug>
 
 DoItYourselfBar::DoItYourselfBar(QObject* parent) : QObject(parent),
-        dbusService(parent) {
+        dbusService(parent),
+        dbusInstanceId(0),
+        dbusError(false),
+        cfg_DBusInstanceId(0) {
 
     QObject::connect(&dbusService, &DBusService::dataPassed,
                      this, &DoItYourselfBar::handlePassedData);
 }
 
+void DoItYourselfBar::cfg_DBusInstanceIdChanged() {
+    auto sessionBus = QDBusConnection::sessionBus();
+    sessionBus.registerService(SERVICE_NAME);
+
+    if (dbusInstanceId != 0) {
+        QString path = "/id_" + QString::number(dbusInstanceId);
+        sessionBus.unregisterObject(path, QDBusConnection::UnregisterTree);
+    }
+
+    if (cfg_DBusInstanceId == 0) {
+        dbusError = true;
+        return;
+    }
+
+    dbusInstanceId = cfg_DBusInstanceId;
+
+    QString path = "/id_" + QString::number(dbusInstanceId);
+    dbusError = !sessionBus.registerObject(path, QString(SERVICE_NAME), &dbusService, QDBusConnection::ExportAllSlots);
+}
+
 void DoItYourselfBar::handlePassedData(QString data) {
+    qDebug() << "Data passed:" << data;
 }
--- plugin/DoItYourselfBar.hpp
+++ plugin/DoItYourselfBar.hpp
@@ -10,8 +10,21 @@ class DoItYourselfBar : public QObject {
 public:
     DoItYourselfBar(QObject* parent = nullptr);
 
+    Q_PROPERTY(bool dbusError
+               MEMBER dbusError);
+
+    Q_PROPERTY(unsigned cfg_DBusInstanceId
+               MEMBER cfg_DBusInstanceId
+               NOTIFY cfg_DBusInstanceIdChanged)
+
+    void cfg_DBusInstanceIdChanged();
+
 private:
     DBusService dbusService;
+    unsigned dbusInstanceId;
+
+    bool dbusError;
+    unsigned cfg_DBusInstanceId;
 
     void handlePassedData(QString data);
 };
