b8eb84bed9ef7f9e01bffa5cd61b98943d027bf3
wsdfhjxc <wsdfhjxc@op.pl>
2020-11-09 03:01:47 +0100

    Update example script

--- examples/network-volume-battery.sh
+++ examples/network-volume-battery.sh
@@ -4,12 +4,17 @@ ID=$1
 
 while true
 do
-    NET=$(nmcli -f STATE -t g)
+    [[ $(nmcli -f STATE -t g) == "connected" ]] && {
+        NET="CONNECTED"
+    } || {
+        NET="DISCONNECTED"
+    }
+
     VOL=$(amixer -D pulse sget Master | grep -Po "\[\K(\d+)" | head -n 1)
     BATT=$(cat /sys/class/power_supply/BAT*/capacity)
 
     DATA=""
-    DATA+="| A | ${NET^^} | | |"
+    DATA+="| A | $NET | | |"
     DATA+="| B | VOL $VOL% | | |"
     DATA+="| C | BATT $BATT% | | |"
 
